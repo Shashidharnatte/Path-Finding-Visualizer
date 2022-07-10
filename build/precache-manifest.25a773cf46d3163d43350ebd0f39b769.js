7fc14f4f9e50c40f3e0689eb9217354432a3dc7f
Natte Shashidhar <95182861+Shashidharnatte@users.noreply.github.com>
2022-07-10 19:55:14 +0530

    Add files via upload

---
+++ build/precache-manifest.25a773cf46d3163d43350ebd0f39b769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f106fa3259cbacfec79b2e1c13780f7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f5767118cbfb9a7dae5",
+    "url": "/static/css/main.3944e690.chunk.css"
+  },
+  {
+    "revision": "d57684a2455f1b6710f1",
+    "url": "/static/js/2.65cfc8ef.chunk.js"
+  },
+  {
+    "revision": "7f5767118cbfb9a7dae5",
+    "url": "/static/js/main.da3b5f8a.chunk.js"
+  },
+  {
+    "revision": "d6c8b0338a16e1e82302",
+    "url": "/static/js/runtime-main.60d0dddc.js"
+  }
+]);
